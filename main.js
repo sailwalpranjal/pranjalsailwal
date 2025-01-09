ce9351e012cc330124f21cdad92c1ed573be50fd
Pranjal Sailwal <127615786+sailwalpranjal@users.noreply.github.com>
2025-01-10 01:32:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 import './style.css'
 import Experience from './Experience/Experience.js';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 const experience = new Experience(document.querySelector(".experience-canvas"));
